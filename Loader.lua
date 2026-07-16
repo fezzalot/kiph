b867a6025eeba7ee98675f400271dcd0d61ac4ce
Fezza <cyrustwashere@gmail.com>
2026-07-15 19:53:10 -0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,15 @@
+local suc, res = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/fezzalot/kiph/refs/heads/main/places/" .. game.PlaceId .. ".lua")
+end)
+if suc then
+    loadstring(res)()
+else
+    if not shared.dontShowAgain then
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "kliph",
+            Text = "Unsupported game! Find supported games in: https://discord.gg/QhKyfqSZTn",
+            Duration = 10
+        })
+        shared.dontShowAgain = true
+    end
+end
